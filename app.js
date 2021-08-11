628244631adadd88498ddee54ce37734e886e23e
VR <vitrayev@gmail.com>
2021-08-11 14:39:16 -0400

    created function for player button selection, added cpu/player choice functions to onclick event

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-cpuScore = 0;
-playerScore = 0;
+let cpuScore = 0;
+let playerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    return computerSelection;
-}
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+computerPlay(1, 3);
 
 function computerPlay(min, max){
     let selection = Math.round(Math.random() * (max - min) + min);
-    if (selection === 1){
-        return "rock";
-    }else if (selection === 2){
-        return "paper";
-    }else return "scissors";
+    if (selection === rock){
+        return computerSelection = 1;
+    }else if (selection === paper){
+        return computerSelection = 2;
+    }else return computerSelection = 3;
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay(1, 3);
-console.log(playRound(playerSelection, computerSelection));
+ function playerChoice(choice) {
+     console.log(choice), console.log(computerSelection);
+    if (choice === "rock"){
+     if (computerSelection === paper) {
+         console.log("Computer chose Paper, You Lose!");
+     } else if (computerSelection === scissors){
+         console.log("Computer chose Scissors, You Win!");
+     } else console.log("You both chose Rock! It's a tie!");
+
+    }else if (choice === "paper"){
+     if (computerSelection === rock) {
+         console.log("Computer chose Rock, You Win!"); 
+     } else if (computerSelection === scissors){
+         console.log("Computer chose Scissors, You Lose!"); 
+     } else console.log("You both chose Paper! It's a tie!");
+       
+    }else 
+     if (computerSelection === rock) {
+        console.log("Computer chose Rock, You lose!");
+     } else if (computerSelection === paper){
+        console.log("Computer chose Paper, You win!");
+     } else console.log("You both chose Scissors! It's a tie!");
+}
+
